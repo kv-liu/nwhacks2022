64f5db30129c95081d5896630788eeaaea0a4d78
na-wu <nwu1018@gmail.com>
2022-01-15 18:21:41 -0800

    rser

--- tldrr/content.js
+++ tldrr/content.js
@@ -13,3 +13,37 @@ document.getElementById("button-id-1").addEventListener("click", function() {
         console.log('The buttons state is currently ' + result.button);
       });
   });
+const tldr = ['TLDR', 'TL;DR', 'TLDR;', 'TL; DR', ';TLDR', 'TLDR:', 'TL;DR:']
+const AWS_API = 'https://yhbz7ctiy5.execute-api.us-west-1.amazonaws.com/api/v1/summarize'
+fetch(window.location.href + '.json')
+.then(response => response.json())
+.then(data => {
+    const text = data[0].data.children[0].data.selftext
+    var result;
+
+    tldr.forEach((el) => {
+        if(!text.includes(el)) {
+            console.log(text);
+            result = text;
+        }
+    })
+
+    var payload = {
+        "prompt": result
+    }
+    // send to API
+    fetch(AWS_API, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(payload)
+    }).then(response => response.json()).then(data => { console.log(data) }).catch((err) => console.log(err))
+});
+
+
+
+// check if current post has TLDR
+// If not, parse DOM for body and make API request for TLDR
+// Inject new TLDR into body with button
+// Add event listener to button connected to localStorage
