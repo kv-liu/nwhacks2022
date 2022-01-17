28a00d3738cdb2499f48297115b0761591aa6721
unknown <kevinliu8260@hotmail.com>
2022-01-16 21:03:35 -0800

    delete reading entry JS function

--- tldrr/popup.js
+++ tldrr/popup.js
@@ -1,7 +1,21 @@
 
 fetchReadingList()
-document.getElementById('delBtn').addEventListener("click", removeEntry);
+//document.getElementById('delBtn').addEventListener("click", function(){ removeEntry(this); });
 document.getElementById('clearBtn').addEventListener("click", clearList);
+
+var removalList = document.getElementsByClassName('delBtn');
+
+for (var i = 0; i < removalList.length; i++) {
+  (function(index) {
+    
+    removalList[index].addEventListener("click", function() {
+      console.log("Ok");
+      {removeEntry(this)};
+    })
+  })(i);
+}
+
+
 
 function fetchReadingList() {
   console.log("fetching reading list...")
@@ -17,15 +31,18 @@ function fetchReadingList() {
       var title = readingListItem.title
       var link = readingListItem.url
 
+      var liTag = document.createElement('li');
+      liTag.setAttribute('class', 'flex items-center hover:bg-gray-800')
+      liTag.setAttribute('id', readingListItem.compositeKey);
+      // entry.appendChild(document.createTextNode(items[key]));
+
       var aTag = document.createElement("a");
       aTag.setAttribute('href', link); //URL
       aTag.setAttribute('target', '_blank'); //URL
-      aTag.setAttribute('id', readingListItem.compositeKey);
+      aTag.setAttribute('style', "display:block; width: 369px;");
 
-
-      var liTag = document.createElement('li');
-      liTag.setAttribute('class', 'py-4 hover:bg-gray-800')
-      // entry.appendChild(document.createTextNode(items[key]));
+      var div = document.createElement('div')
+      div.setAttribute('class', 'py-4 hover:bg-gray-800')
 
       var div1 = document.createElement('div')
       div1.setAttribute('class', 'flex ml-3 items-center')
@@ -45,32 +62,35 @@ function fetchReadingList() {
       p2.innerText = link
 
 
-      var form = document.createElement('form');
-      form.setAttribute('class', 'h-3 w-3 mr-3 ml-auto');
-
       var btn = document.createElement('button');
-      btn.setAttribute('id', 'delBtn');
       btn.setAttribute('style', 'filter:brightness(0) invert(1);');
+      btn.setAttribute('class', 'delBtn h-3 w-3 mr-1 ml-auto items-center'); //class = delBtn
+
+      btn.addEventListener("click", function() {
+        console.log("Ok2");
+        {removeEntry(this)};
+      })
 
       var img2 = document.createElement('img');
       img2.src = '/images/x.png';
       
+      
+      list.appendChild(liTag);
+      liTag.appendChild(aTag);
+      aTag.appendChild(div);
+      div.appendChild(div1);
 
-      aTag.appendChild(liTag);
-      liTag.appendChild(div1);
       div1.appendChild(img);
       div1.appendChild(div2);
-      div1.appendChild(form);
-      form.appendChild(btn)
-      btn.appendChild(img2);
+
       div2.appendChild(p1);
       div2.appendChild(p2);
-      list.appendChild(aTag);
+
+      liTag.appendChild(btn)
+      btn.appendChild(img2);
     }
   })
 }
-
-
 
 function clearList() {
   console.log("Clearing storage....");
@@ -80,28 +100,41 @@ function clearList() {
 
 
 function removeEntry(el) {
-  var listItemId = el.parentElement.parentElement.parentElement.parentElement.id
+
+  
+  console.log("made it to removeEntry function");
+  console.log(el)
+  console.log(el.parentElement.nodeName)
+  var listItemId = el.parentElement.id
+
   chrome.storage.local.get(function(res) {
     var filtered = res.tldrReadingList.filter(e => e.compositeKey != listItemId)
+    console.log("Here")
+    console.log(listItemId);
+    console.log(filtered);
+    console.log("end")
     chrome.storage.local.set({ 'tldrReadingList': filtered })
-    fetchReadingList()
+    console.log(res);
+
+     fetchReadingList()
   })
 }
 
-{/* <a href="https://www.google.com" target='_blank'>
-<li class="py-4 hover:bg-gray-800">
-  <div class="flex flex-wrap ml-3 items-center">
-    <img src="./images/reddit-logo-512x512.png" class="h-7 w-7">
-    <div class='ml-5'>
-      <p class="font-medium text-gray-100">This is the title.</p>
-      <p class="text-gray-300">This is the URL.</p>
+{/* <li class='flex items-center' id='test-id'>
+<a href="#" target='_blank' style='display:block; width: 350px;'>
+  <div class="py-4 hover:bg-gray-800">
+    <div class="flex ml-3 items-center">
+      <img src="./images/reddit-logo-512x512.png" class="h-7 w-7">
+      <div class='ml-3'>
+        <p class="font-medium text-gray-100">This is the title.</p>
+        <p class="text-gray-300">This is the URL.</p>
+      </div>
     </div>
-
-    <form class='h-3 w-3 mr-3 ml-auto'>
-      <button id='delBtn' style='filter:brightness(0) invert(1);'>
-        <img src='./images/x.png'>
-      </button>
-    </form>
   </div>
-</li>
-</a> */}
+</a>
+
+<button class='h-3 w-3 mr-2 ml-auto items-center' type='button' id='delBtn' style='border:none; filter:brightness(0) invert(1); display:inline-block;'>
+  <img src='./images/x.png'>
+</button>
+
+</li> */}
